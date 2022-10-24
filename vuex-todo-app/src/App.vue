4675403898853a127020a374b29277653d492fa2
Jhom <jhomwan238@gmail.com>
2022-10-24 23:45:09 +0700

    first commit

---
+++ vuex-todo-app/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="container">
+    <TodoLists />
+  </div>
+</template>
+
+<script>
+import TodoLists from "./components/TodoLists.vue";
+
+export default {
+  name: "App",
+  components: {
+    TodoLists,
+  },
+};
+</script>
+
+<style>
+.container {
+  max-width: 1100px;
+  margin: auto;
+  overflow: auto;
+  padding: 0 2rem;
+}
+</style>
---
+++ vuex-todo-app/src/store/index.js
@@ -0,0 +1,10 @@
+import { createStore } from "vuex";
+
+import todos from "./modules/todos";
+
+//Create Store
+export default createStore({
+  modules: {
+    todos,
+  },
+});
