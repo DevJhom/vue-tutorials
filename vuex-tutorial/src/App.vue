b87a0edebbd60858a2a013340dd3f9f93eee6050
Jhom <jhomwan238@gmail.com>
2022-10-26 00:57:16 +0700

    state, getters, mutations

--- vuex-tutorial/src/App.vue
+++ vuex-tutorial/src/App.vue
@@ -1,14 +1,34 @@
 <template>
-  <base-container title="Vuex"></base-container>
+  <base-container title="Vuex">
+  <TheCounter/>
+  <FavoriteValue/>
+  <button @click="addOne">Add 5</button>
+  <ChangeCounter/>
+  </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
+import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
+import FavoriteValue from './components/FavoriteValue.vue';
 
 export default {
   components: {
     BaseContainer,
+    TheCounter,
+    ChangeCounter,
+    FavoriteValue
   },
+  methods: {
+    addOne() {
+      // this.$store.commit('increase', {value: 5});
+      this.$store.commit({
+        type: 'increase',
+        value: 10
+      })
+    }
+  }
 };
 </script>
 
