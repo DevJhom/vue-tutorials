999910f71fb72c8f2fef95d0883600eefc0d6393
Jhom <jhomwan238@gmail.com>
2022-11-10 23:56:25 +0700

    warming up vue basics

--- vue-cdn/app.js
+++ vue-cdn/app.js
@@ -1,8 +1,19 @@
 const app = Vue.createApp({
     data() {
         return {
-            courseGoal: "Master Vue.js because it's awesome!"
+            vueLink: "https://vuejs.org/guide/introduction.html",
+            randomNumber: 0,
+            name: "Listening to input event"
         };
+    },
+    methods: {
+        randomize(digit) {
+            this.randomNumber = Math.round(Math.random() * digit);
+            return randomNumber;
+        },
+        setName(e){
+            this.name = e.target.value;
+        }
     }
 });
 
