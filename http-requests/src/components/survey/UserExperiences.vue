1f4672afc289dec7dfc234434eeeb28a8dfd823f
Jhom <jhomwan238@gmail.com>
2022-11-18 23:26:43 +0700

    fetching http requests

---
+++ http-requests/src/components/survey/UserExperiences.vue
@@ -0,0 +1,76 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
+      </div>
+      <ul v-if="!isLoading">
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+      <ul v-else>
+        Loading
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  // props: ['results'],
+  components: {
+    SurveyResult,
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false,
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      fetch(
+        'https://dummy-database-71242-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json'
+      )
+        .then((res) => {
+          if (res.ok) {
+            return res.json();
+          }
+        })
+        .then((data) => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating,
+            });
+          }
+          this.results = results;
+        });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
+  },
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
