946d0d4b7d31cc7c0dd07a4326bb5bffce8cbdd9
Jhom <jhomwan238@gmail.com>
2022-11-23 22:46:41 +0700

    vue routing basics

---
+++ vue-routing/src/main.js
@@ -0,0 +1,41 @@
+import { createApp } from 'vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+import App from './App.vue';
+import TeamsList from './components/teams/TeamsList';
+import UsersList from './components/users/UsersList';
+import TeamMembers from './components/teams/TeamMembers';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/teams'
+    },
+    {
+      path: '/teams',
+      component: TeamsList,
+    },
+    {
+      path: '/users',
+      component: UsersList,
+    },
+    {
+      path: '/teams/:teamId',
+      component: TeamMembers,
+      props: true,
+    },
+    {
+     path: '/:notFound(.*)',
+     redirect: '/'
+    }
+  ],
+  linkActiveClass: 'active',
+});
+
+const app = createApp(App);
+
+app.use(router);
+
+app.mount('#app');
