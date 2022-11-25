f56924690a34cdbf635cb7da1d1a8420f827dfb6
Jhom <jhomwan238@gmail.com>
2022-11-25 17:55:13 +0700

    more on the composition api

--- composition-api/src/App.vue
+++ composition-api/src/App.vue
@@ -1,48 +1,77 @@
 <template>
   <section class="container">
-    <h2>{{ userName }}</h2>
-    <h2>{{ userAge }}</h2>
+    <user-data :firstname="firstName" :lastname="lastName"></user-data>
     <button @click="setAge">Change Age</button>
+    <br><br>
     <div>
       <input type="text" placeholder="First Name" v-model="firstName" />
-      <input type="text" placeholder="Last Name" v-model="lastName" />
+      <!-- <input type="text" placeholder="Last Name" v-model="lastName" /> -->
+      <input type="text" placeholder="Last Name" ref="lastNameInput" />
+      <button @click="setLastName">Set Last Name</button>
     </div>
   </section>
 </template>
 
 <script>
-import { ref, computed, watch } from 'vue';
-// import { reactive } from 'vue';
+import { ref, reactive, computed, watch, provide } from 'vue';
+import UserData from './components/UserData.vue'
 
 export default {
+  components: {
+    UserData
+  },
   setup() {
-    // const user = reactive({
-    //   name: 'Micheal',
-    //   age: 21,
-    // }
+    const userAge = ref('21');
     const firstName = ref('');
     const lastName = ref('');
-    const userAge = ref('21');
+    const lastNameInput = ref(null);
 
-    const userName = computed(function () {
-      return firstName.value + '' + lastName.value;
+    const user = reactive({
+      name: 'Micheal',
+      age: 21,
     });
 
-    watch([userName, userAge], function (newValues, oldValues) {
-      console.log(newValues);
-      console.log(oldValues);
+    // setTimeout(function () {
+    //   userName.value = 'Micheal Jackson';
+    //   userAge.value = '40';
+
+    //   user.name = 'Micheal Jackson';
+    //   user.age = '40';
+
+    // }, 2000);
+
+    const userName = computed(function () {
+      return firstName.value + ' ' + lastName.value;
     });
 
     function setAge() {
-      userAge.value = 32;
+      userAge.value = '40';
     }
 
+    function setLastName(){
+      lastName.value = lastNameInput.value.value;
+    }
+
+    //watch takes 2 arguments - a dependency and a function
+    //the function also receives 2 arguments - new value and old value
+    watch([firstName, lastName], function (newValue, oldValue) {
+      console.log(newValue);
+      console.log(oldValue);
+    });
+
+    //provide() takes 2 arguments - name and value
+    provide('userAge', userAge);
+
     return {
-      userName,
-      userAge,
+      // userName,
+      // userAge,
+      user,
       setAge,
+      userName,
       firstName,
       lastName,
+      setLastName,
+      lastNameInput
     };
   },
 };
---
+++ composition-api/src/components/UserData.vue
@@ -0,0 +1,55 @@
+<template>
+  <h2>{{ username }}</h2>
+  <h3>{{ age }}</h3>
+</template>
+
+<script>
+import { computed, inject } from 'vue';
+import {
+  onBeforeMount,
+  onMounted,
+  onBeforeUpdate,
+  onUpdated,
+  onBeforeUnmount,
+  onUnmounted,
+} from 'vue';
+
+export default {
+  props: ['firstname', 'lastname'],
+  setup(props, context) {
+    const username = computed(function () {
+      return props.firstname + ' ' + props.lastname;
+    });
+
+    console.log(context);
+    context.emit('save-data', 1);
+
+    const age = inject('userAge');
+    console.log(age);
+
+    onBeforeMount(function () {
+      console.log('onBeforeMount');
+    });
+    onMounted(function () {
+      console.log('onMounted');
+    });
+    onBeforeUpdate(function () {
+      console.log('onBeforeUpdate');
+    });
+    onUpdated(function () {
+      console.log('onUpdated');
+    });
+    onBeforeUnmount(function () {
+      console.log('onBeforeUnmount');
+    });
+    onUnmounted(function () {
+      console.log('onUnmounted');
+    });
+
+    return {
+      username,
+      age,
+    };
+  },
+};
+</script>
