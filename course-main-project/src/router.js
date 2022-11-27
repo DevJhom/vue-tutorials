1ee45a298425b2379914e6fd0afc645a723733e2
Jhom <jhomwan238@gmail.com>
2022-11-27 19:42:26 +0700

    course project: initial commit

---
+++ course-main-project/src/router.js
@@ -0,0 +1,46 @@
+import { createRouter, createWebHistory } from 'vue-router';
+
+import CoachesList from './pages/coaches/CoachesList.vue';
+import CoachDetails from './pages/coaches/CoachDetails.vue';
+import CoachRegisteration from './pages/coaches/CoachRegisteration.vue';
+import ContactCoach from './pages/requests/ContactCoach.vue';
+import RequestsReceived from './pages/requests/RequestsReceived.vue';
+import NotFound from './pages/NotFound.vue';
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      redirect: '/coaches',
+    },
+    {
+      path: '/coaches',
+      component: CoachesList,
+    },
+    {
+      path: '/coaches/:id',
+      component: CoachDetails,
+      children: [
+        {
+          path: 'contact', // /coaches/c1/contact
+          component: ContactCoach,
+        },
+      ],
+    },
+    {
+      path: '/register',
+      component: CoachRegisteration,
+    },
+    {
+      path: '/requests',
+      component: RequestsReceived,
+    },
+    {
+      path: '/:notFound(.*)',
+      component: NotFound,
+    },
+  ],
+});
+
+export default router;
